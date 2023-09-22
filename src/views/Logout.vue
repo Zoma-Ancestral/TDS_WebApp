f869fa43b037ba6b08cc5b863e1262d9f7d17ccf
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2023-09-22 14:37:34 +0200

    Création Vue log in et log out

---
+++ src/views/Logout.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { useRouter } from 'vue-router';
+  import { getAuth, signOut } from 'firebase/auth';
+
+  const router = useRouter()
+
+  const auth = getAuth()
+  signOut(auth)
+    .then(() => {
+      console.log("Log out complete")
+      router.push("/")
+    })
+    .catch(error => {
+      console.log(error.code)
+      alert(error.message)
+    })
+  
+</script>
