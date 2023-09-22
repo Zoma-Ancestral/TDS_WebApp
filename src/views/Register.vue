e9a134b6578abbd8ba8742c25341befeeb50e55b
Zoma-Ancestral <34089184+Zoma-Ancestral@users.noreply.github.com>
2023-09-22 13:01:34 +0200

    commit
    Mis le template en bas du fichier pour suivre l'organisation des autres .vue

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,10 +1,3 @@
-<template>
-  <h1>Create an Account</h1>
-  <p><input type="text" placeholder="Email" v-model="email" /></p>
-  <p><input type="password" placeholder="Password" v-model="password" /></p>
-  <p><button @click="register">Submit</button></p>
-</template>
-
 <script setup>
   import { ref } from 'vue'
   import { useRouter } from 'vue-router';
@@ -26,3 +19,10 @@
       })
   }
 </script>
+
+<template>
+  <h1>Create an Account</h1>
+  <p><input type="text" placeholder="Email" v-model="email" /></p>
+  <p><input type="password" placeholder="Password" v-model="password" /></p>
+  <p><button @click="register">Submit</button></p>
+</template>
